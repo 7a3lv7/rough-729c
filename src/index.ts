e305a0365e51cfca0355316a464c786d098b0c92
7a3lv7 <hjkl931031@gmail.com>
2025-04-16 07:35:06 +0000

    post and check inputs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 export default {
   async fetch(request, env) {
+    if (request.method !== "POST") {
+      return new Response("This request is not supported", { status: 405, headers: { Allow: "POST" } });
+    }
     const inputs = await request.json() as AiTextToImageInput;
-
+    if (!inputs.prompt) {
+      return new Response("missing prompt", { status: 400 });
+    }
     const response = await env.AI.run(
       "@cf/stabilityai/stable-diffusion-xl-base-1.0",
       inputs,
